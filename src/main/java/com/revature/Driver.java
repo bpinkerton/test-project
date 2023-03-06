28cc29aaa7f7428ec442906c9f236824a9348b24
Brandon Pinkerton <brandon.a.pinkerton@gmail.com>
2023-03-06 14:22:59 -0600

    add src files for git demo

---
+++ src/main/java/com/revature/Driver.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+public class Driver {
+    public static void main(String[] args) {
+        // check the length
+
+        if(args.length > 0)
+            System.out.println("Hello world from " + args[0]);
+        else
+            System.out.println("Hello world.");
+    }
+}
